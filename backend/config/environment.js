ddd9e50cf46a5bf743b65ea8e6fd3da72c4347a3
Sarah Wood <wood.sarah@outlook.com>
2020-01-27 18:47:45 +0000

    Ready for deployment

--- backend/config/environment.js
+++ backend/config/environment.js
@@ -1,8 +1,8 @@
 // Global environment variables we need in our server
 const port = 8000
 const dbURIPrefix = process.env.MONGODB_URI || 'mongodb://localhost/'
-const dbName = 'heroku_rmqr8mh5'
-const dbURI = `${dbURIPrefix}${dbName}`
+const dbName = 'the-vault'
+const dbURI = `${dbURIPrefix}`
 // our secret used for encoing our JWT tokens, used in '/controllers/user' and '/lib/secureRoute'
 const secret = 'This is my really secret string that nobody is going to be able to guess1'
 
