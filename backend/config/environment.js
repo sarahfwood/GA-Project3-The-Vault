1df69c64ac147539d8a7c8661a7c28f73bcc1b67
Sarah Wood <wood.sarah@outlook.com>
2020-01-27 18:19:07 +0000

    Ready for deployment

--- backend/config/environment.js
+++ backend/config/environment.js
@@ -1,6 +1,6 @@
 // Global environment variables we need in our server
 const port = 8000
-const dbURIPrefix = process.env.MONGODB_URI || 'mongodb://localhost/'
+const dbURIPrefix = process.env.MONGOLAB_MAROON_URI || 'mongodb://localhost/'
 const dbName = 'the-vault'
 const dbURI = `${dbURIPrefix}${dbName}`
 // our secret used for encoing our JWT tokens, used in '/controllers/user' and '/lib/secureRoute'
