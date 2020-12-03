071f99cf8650a123cda8b7394ba5d108af04d8dd
Sarah Wood <wood.sarah@outlook.com>
2020-12-03 21:33:07 +0000

    cluster added

--- backend/config/environment.js
+++ backend/config/environment.js
@@ -1,15 +1,7 @@
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://heroku_rmqr8mh5:thevault1@cluster-rmqr8mh5.vljhs.mongodb.net/admin?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-
+const uri = process.env.MONGODB_URI;
 // Global environment variables we need in our server
 const port = process.env.PORT || 8000
-const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/the-vault'
+// const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/the-vault'
 // our secret used for encoing our JWT tokens, used in '/controllers/user' and '/lib/secureRoute'
 const secret = 'This is my really secret string that nobody is going to be able to guess1'
 
